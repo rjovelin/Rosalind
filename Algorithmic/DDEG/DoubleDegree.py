36eb14f27e4f4c7d283345ef887203d5744137a9
rjovelin <richard.jovelin@utoronto.ca>
2019-04-19 17:11:22 -0400

    solved pb

--- Algorithmic/DDEG/DoubleDegree.py
+++ Algorithmic/DDEG/DoubleDegree.py
@@ -5,27 +5,36 @@ Created on Fri Apr 19 01:38:36 2019
 @author: Richard
 """
 
-
-
-# use a dict to store connections:
-# D[i] = {j, k, m}
-
-# iterate through sorted node lists:
-for each neighbor, count edges (len(D[j])) --> sum for i
-
-
-
 def DoubleDegree(PbFile):
     '''
     
     
     '''
     
-    
     # extract data from file
-    
+    infile = open(PbFile)
     NumNodes, NumEdges = list(map(lambda x: int(x), infile.readline().rstrip().split()))
-    
-    
-    # nodes may be missing from list: add 0 connections
-    
+    # extract interactions between nodes
+    interactions = infile.read().strip().split('\n')
+    infile.close()
+    # create a dict to store neighbors of node i D[i] = {j, k, m}
+    D = {}
+    # initialize D with empty sets to account for possible nodes without connection
+    for i in range(1, NumNodes + 1):
+        D[i] = set()
+    # record interactions
+    for i in interactions:
+        i = i.split()
+        D[int(i[0])].add(int(i[1]))
+        D[int(i[1])].add(int(i[0]))
+        
+    # count interactions of each neighbor
+    K = {}
+    for i in D:
+        K[i] = sum([len(D[j]) for j in D[i]])
+    # get a sorted list of nodes
+    nodes = list(K.keys())
+    nodes.sort()
+    count = [str(K[i]) for i in nodes]
+    print(' '.join(count))
+        
