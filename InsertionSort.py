8722106475d8dc560ea9e0e3020c900fe634fd52
rjovelin <richard.jovelin@oicr.on.ca>
2019-04-26 01:20:50 -0400

    solved pb

--- InsertionSort.py
+++ InsertionSort.py
@@ -18,29 +18,16 @@ def InsertionSort(A):
     
     for i in range(1, len(A)):
         k = i
-        while k > 1 and A[k] < A[k-1]:
-            N +=1
+        while k >= 1 and A[k] < A[k-1]:
             # swap A[k] and A[k-1]
             A[k-1], A[k] = A[k], A[k-1]
             k -= 1
             # record number of swap
-    
-
-    for i in range(1, len(A)):
-        k = i
-        while k > 1 and A[k] < A[k-1]:
             N +=1
-            # swap A[k] and A[k-1]
-            A[k-1], A[k] = A[k], A[k-1]
-            k -= 1
-            # record number of swap
 
-         
-            
     return N
 
 
 
 a = [6, 10, 4, 5, 1, 2]
 print(InsertionSort(a))
-print(a)
