ab98d5c7c4ba9e463444adae60f1ed318c7c5c14
Richard Jovelin <RJovelin@ad.oicr.on.ca>
2019-05-03 09:21:09 -0400

    trying parsing a newick tree into a dict

---
+++ readNewick.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  2 22:57:55 2019
+
+@author: rjovelin
+"""
+
+
+
+import re
+
+def parse(newick):
+    tokens = re.findall(r"([^:;,()\s]*)(?:\s*:\s*([\d.]+)\s*)?([,);])|(\S)", newick+";")
+
+    def recurse(nextid = 0, parentid = -1): # one node
+        thisid = nextid;
+        children = []
+
+        name, length, delim, ch = tokens.pop(0)
+        if ch == "(":
+            while ch in "(,":
+                node, ch, nextid = recurse(nextid+1, thisid)
+                children.append(node)
+            name, length, delim, ch = tokens.pop(0)
+        return {"id": thisid, "name": name, "length": float(length) if length else None, 
+                "parentid": parentid, "children": children}, delim, nextid
+
+    return recurse()[0]
+
+# Example use:
+Tree = parse("(A:0.1,B:0.2,(C:0.3,D:0.4)E:0.5,G:0.8)F:0.9")
+
+
+
+
+
+
+def ParseNewick(Tree):
+    
+    tokens = re.findall(r"([^:;,()\s]*)(?:\s*:\s*([\d.]+)\s*)?([,);])|(\S)", Tree+";")
+    
+    def recurse(nextid = 0, parentid = -1): # one node
+        thisid = nextid;
+        children = []
+
+        name, length, delim, ch = tokens.pop(0)
+        if ch == "(":
+            while ch in "(,":
+                node, ch, nextid = recurse(nextid+1, thisid)
+                children.append(node)
+            name, length, delim, ch = tokens.pop(0)
+        return {name: children}, delim, nextid
+
+    return recurse()[0]
+    
